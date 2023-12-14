d4b2e1373db6f64e648d27bbc31c669e5b1effda
MelyEspinosa <meliespinosa490@gmail.com>
2023-12-14 12:54:01 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,34 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Animaciones from "./components/Animaciones";
 import TecnologiasPage from "./pages/TecnologiasPage";
 
-import Carrusel from "./Components/Carrusel"
-import LogoVerde from "./Components/LogoVerde"
-import Navbar from "./Components/Navbar"
+import Carrusel from "./Components/Carrusel";
+import LogoVerde from "./Components/LogoVerde";
+import Navbar from "./Components/Navbar";
+import Navbar_Context from "./components/Navbar_Context";
+import InvernaderoPage from "./pages/InvernaderoPage";
+import { InsoelProvider } from "./Context/InsoelContext";
+import ClientesPage from "./pages/ClientesPage";
+import Nosotros from "./components/Nosotros";
+import NosotrosPage from "./pages/NosotrosPage";
+import ContactanosPage from "./pages/ContactanosPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <main>
-      <LogoVerde />
-      <div>
-      <p className="text-9xl"></p>
-     
-     </div>  
-        <Routes>
-          <Route path="/tecnologias" element={<TecnologiasPage />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <InsoelProvider>
+      <BrowserRouter>
+        <main>
+          <Navbar_Context />
+          <Routes>
+            <Route path="/" element={<LogoVerde />} />
+            <Route path="/tecnologias" element={<TecnologiasPage />} />
+            <Route path="/invernadero" element={<InvernaderoPage />} />
+            <Route path="/clientes" element={<ClientesPage />} />
+            <Route path="/nosotros" element={<NosotrosPage/>} />
+            <Route path="/contactanos" element={<ContactanosPage/>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </InsoelProvider>
   );
 }
 
