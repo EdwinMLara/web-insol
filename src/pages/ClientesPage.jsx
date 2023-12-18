0a8ba1ee27750c0a6b5c9ffcab0cff3e46673577
MelyEspinosa <meliespinosa490@gmail.com>
2023-12-18 13:47:05 -0600

    cambios

--- src/pages/ClientesPage.jsx
+++ src/pages/ClientesPage.jsx
@@ -1,6 +1,6 @@
+import Clientes from '../Components/Clientes'
 import { useInsoel } from '../Context/InsoelContext'
-import Clientes from '../components/Clientes'
-import Nosotros from '../components/Nosotros'
+
 function ClientesPage() {
     const {setTxtColor} = useInsoel()
     setTxtColor('black')
