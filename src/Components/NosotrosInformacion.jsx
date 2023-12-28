34b4b1e8011aab242a5eed132a8e9afaa4b8c1d9
MelyEspinosa <meliespinosa490@gmail.com>
2023-12-28 12:31:30 -0600

    ajuste

--- src/Components/NosotrosInformacion.jsx
+++ src/Components/NosotrosInformacion.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function NosotrosInformacion() {
+  const [numberOfLines, setNumberOfLines] = useState(9);
 
-    const numberOfLines = 9; // Adjust this number as needed
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+
+      // Ajusta el número de líneas según el ancho de la pantalla
+      if (screenWidth < 600) {
+        setNumberOfLines(3);
+      } else {
+        setNumberOfLines(9);
+      }
+    };
+
+    // Agrega un event listener para el cambio de tamaño de la ventana
+    window.addEventListener('resize', handleResize);
+
+    // Limpia el event listener cuando el componente se desmonta
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Asegúrate de ejecutar el efecto solo una vez al montar el componente
+
+
+    //const numberOfLines = 9; // Adjust this number as needed
 
     const verticalLines = Array.from({ length: numberOfLines }).map((_, index) => (
         <React.Fragment key={index}>
@@ -29,9 +52,11 @@ function NosotrosInformacion() {
     <div className='text-center mt-24'>
         <div className='text-white pt-5 p-5 z-10 relative'> "En INSOEL, somos arquitectos del progreso, pioneros en el ámbito de la automatización, donde la innovación se une a la eficiencia. Con un compromiso inquebrantable de dar forma al futuro, aprovechamos el poder de la tecnología de vanguardia para automatizar máquinas y programas, trascendiendo límites y redefinir lo que es posible. Nuestra incesante búsqueda de la excelencia permite a las industrias evolucionar, integrando perfectamente la automatización en el latido del progreso.  Donde cada línea de código y cada máquina revoluciona la forma en que funciona el mundo <br /><br />
         INSOEL — Automatización redefinida".
+        <div className='overflow-x-auto'>
         <div className="flex items-end pt-[2rem]">
         {verticalLines}
       </div>
+        </div>
         </div>
         {/* Vertical Lines with Different Sizes */}
  
