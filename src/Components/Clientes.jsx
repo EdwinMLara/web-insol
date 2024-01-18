d0de652c3e90fa75d32d043e768e8d49f0282afa
MelyEspinosa <meliespinosa490@gmail.com>
2024-01-18 14:14:35 -0600

    cambios en el color de la img de clientes

--- src/Components/Clientes.jsx
+++ src/Components/Clientes.jsx
@@ -21,21 +21,21 @@ function Clientes() {
 
 
   return (
-    <div className=" mt-24 mb-10">
+    <div className=" mt-24 mb-10 ">
       <div className=" font-bold text-3xl text-center p-5">
         MARCAS QUE CONFÍAN EN NOSOTROS
-        <div className="flex flex-wrap brightness-50 lur-none justify-center text-center pt-20 pb-5 md:pr-[10rem] md:pl-[10rem]">
+        <div className="relative flex flex-wrap justify-center text-center pt-20 pb-5 md:pr-[10rem] md:pl-[10rem] ">
           <div className="md:flex-none w-1/5 ">
-            <img src={Ug} alt="Imagen 1" className="w-[7rem] h-[7rem]" />
+            <img src={Ug} alt="Imagen 1" className="w-[7rem] h-[7rem] " />
           </div>
           <div className="flex-none w-1/5">
-            <img src={TecM} alt="Imagen 2" className="w-[7rem] h-[7rem]" />
+            <img src={TecM} alt="Imagen 2" className="w-[7rem] h-[7rem] object-cover rounded-full" />
           </div>
           <div className="flex-none w-1/5">
             <img src={Ing} alt="Imagen 3" className="w-[7rem] h-[7rem]" />
           </div>
           <div className="flex-none w-1/5">
-            <img src={Uat} alt="Imagen 4" className="w-[7rem] h-[7rem]" />
+            <img src={Uat} alt="Imagen 4" className="w-[7rem] h-[7rem] " />
           </div>
           <div className="flex-none w-1/5">
             <img src={Solena} alt="Imagen 5" className="w-[24rem] h-[7rem]" />
