5ba339cdac61427709af4f5574bcd4ff338d7edf
Jesus <jesus.salazar.v35@gmail.com>
2024-02-26 13:10:36 -0600

    coemnatario en auth.js para validar error *se descarta*

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,3 +5,4 @@ export const registerRequest = (usuario) => axios.post('/register', usuario)
 export const loginRequest = usuario => axios.post('/login', usuario)
 
 export const verifyTokenRequest = () => axios.get('/verify')
+/**autentificar el usuario */
