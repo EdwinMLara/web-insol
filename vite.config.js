cf19470f705072c6212bf6543b43e15a1fabc2f4
EdwinMLara <emlara35@gmail.com>
2024-04-09 23:33:41 -0600

    cambio para que se pueda desplegar en amplfy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/web-insol/"
-})
+  bbase: "/", // Ensure the base path is correct; it's usually correct by default
+  build: {
+    outDir: "dist", // Specify your output directory if you prefer something different than the default 'dist'
+    // Any other specific configurations needed for your project
+  },
+});
